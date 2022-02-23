80d27f10dfd2638ac7b7d09a3ef1437b8a4af93f
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-02-23 23:36:22 +0300

    Added appbar search

--- lib/Screens/search_user.dart
+++ lib/Screens/search_user.dart
@@ -8,10 +8,37 @@ class UserSearch extends StatefulWidget {
 }
 
 class _UserSearchState extends State<UserSearch> {
+  bool isSearching = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('test'), actions: []),
+      appBar: isSearching
+          ? AppBar(
+              // The search area here
+              backgroundColor: Colors.black,
+              title: Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5)),
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: () {
+                            /* Clear the search field */
+                          },
+                        ),
+                        hintText: 'Search...',
+                        border: InputBorder.none),
+                  ),
+                ),
+              ))
+          : AppBar(
+              backgroundColor: Colors.black, title: Text('test'), actions: []),
     );
   }
 }
